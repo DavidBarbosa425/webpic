7e5656f9bf9dd730e81efc6393dd181e13142262
David <davidbarbosa425@gmail.com>
2022-12-21 20:13:23 -0300

    componentizacao msg de erro

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+import { VmessageModule } from './../photos/photo-list/search/vmessage/vmessage.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SigninComponent } from './signin/signin.component';
@@ -7,7 +8,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
     declarations: [SigninComponent],
     imports: [ReactiveFormsModule,
-              CommonModule]
+              CommonModule,
+            VmessageModule]
 })
 
 export class HomeModule {
---
+++ src/app/photos/photo-list/search/vmessage/vmessage.module.ts
@@ -0,0 +1,12 @@
+import { VmessageComponent } from './vmessage-component';
+import { NgModule } from '@angular/core';
+
+
+@NgModule({
+    declarations: [VmessageComponent],
+    exports: [VmessageComponent]
+})
+
+export class VmessageModule {
+
+}
