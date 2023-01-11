6bb75c783efb6cc2a36945f1c8a70d928f48be70
David <davidbarbosa425@gmail.com>
2023-01-11 16:01:02 -0300

    menu hamburger

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,3 +1,4 @@
+import { ShowIfLoggedModule } from './../shared/directives/show-if-logged/show-if-logged.module';
 import { LoadingModule } from './../shared/components/loading/loading.module';
 import { DarkenOnHoverModule } from './../shared/directives/darken-on-hover/darken-on-hover.module';
 import { AlertModule } from './../shared/components/alert/alert.module';
@@ -8,6 +9,7 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RequestInterceptor } from './auth/request.interceptor';
+import { MenuModule } from '../shared/components/menu/menu.module';
 
 @NgModule({
     declarations: 
@@ -21,7 +23,10 @@ import { RequestInterceptor } from './auth/request.interceptor';
         RouterModule,
         AlertModule,
         DarkenOnHoverModule,
-        LoadingModule],
+        LoadingModule,
+        MenuModule,
+        ShowIfLoggedModule
+        ],
     providers: [
         {
             provide: HTTP_INTERCEPTORS,
--- src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
+++ src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -7,6 +7,8 @@ import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
 
 export class ShowIfLoggedDirective implements OnInit {
 
+    currentDisplay!: string
+
     constructor(
         private elementRef:ElementRef<any>,
         private renderer2: Renderer2,
@@ -14,9 +16,18 @@ export class ShowIfLoggedDirective implements OnInit {
     ) {}
 
     ngOnInit(): void {
-        if(!this.userService.isLogged()) {
-            this.renderer2.setStyle(this.elementRef.nativeElement,'display', 'none')
-        }
+
+        this.currentDisplay = getComputedStyle(this.elementRef.nativeElement).display
+
+        this.userService.getUser()
+            .subscribe(user => {
+                if(user){
+                    this.renderer2.setStyle(this.elementRef.nativeElement, 'display', this.currentDisplay)
+                }else{
+                    this.currentDisplay = getComputedStyle(this.elementRef.nativeElement).display
+                    this.renderer2.setStyle(this.elementRef.nativeElement,'display', 'none')
+                }
+            })
     }
 
 }
